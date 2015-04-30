d5f43d259fce40aaca259f0788e3dffb692ca5bb
Mohammed Sadiq <sadiqmmm@gmail.com>
2015-04-30 09:56:00 +0000

    Root routes should be first

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
-  get 'static_pages/about'
+  get 'static_pages/about'  
 
-  root to: 'static_pages#home'
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
