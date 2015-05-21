b20f567926d0677cefb748115f3177db26d47f43
Mohammed Sadiq <sadiqmmm@gmail.com>
2015-05-21 00:43:34 +0000

    Add the user controller and new method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root to: 'static_pages#home'
 
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about'=> 'static_pages#about'  
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'user#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
